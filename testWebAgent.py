750290901038e0a0ccf1e5cc02aacccd96d83b38
Shivam Garg <sgarg.ma@gmail.com>
2025-09-23 23:34:01 -0400

    updating test script to reflect prev change

--- testWebAgent.py
+++ testWebAgent.py
@@ -1,8 +1,8 @@
 from webAgent import WebAgent
 
 agent = WebAgent()
-agent.go_to_url("https://books.toscrape.com")
+agent.go_to_url("https://books.toscrape.com", "div.page_inner")
 agent.take_screenshot("data/task1/step1_homepage.png")
-agent.click_element(".product_pod h3 a")
+agent.click_element(".product_pod h3 a", "div.page_inner")
 agent.take_screenshot("data/task1/step2_book_detail.png")
 agent.quit()
